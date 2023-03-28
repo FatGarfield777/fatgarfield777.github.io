551dac1c999af0774caac00067b7b9e9676dd2c5
FatGarfield777 <Felixschwarz2012@gmail.com>
2023-03-27 21:07:26 -0400

    gamer

---
+++ auth.js
@@ -0,0 +1,7 @@
+var auth0 = new auth0.WebAuth({
+  domain: 'fatgarfield777.us.auth0.com',
+  clientID: 'okqJvRUru9qpT6BMLIRqtstkB3O1cE4p',
+  redirectUri: 'https://www.fatgarfield777.xyz/callback',
+  responseType: 'token id_token',
+  scope: 'openid profile email'
+});
